15165dd9a3906d0325a69d89a37de8d227779cbd
dominiceggerman <dominiceggerman@gmail.com>
2018-10-17 11:57:39 -0600

    Added utility to resume old run (WIP), and to save old query.

--- forceFetchBot.py
+++ forceFetchBot.py
@@ -3,6 +3,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import datetime
+import argparse
+import os
 
 # Generate date range
 def dateRangeGenerator():
@@ -52,25 +54,60 @@ def tableChecker():
 
 # Run
 if __name__ == '__main__':
+    # Argparse and add arguments
+    parser = argparse.ArgumentParser(description="Below is a list of optional arguements with descriptions. Please refer to README.md for full documentation and examples...")
+    parser.add_argument("-r", "--res", help="Resume last query", action="store_true")
+    options = parser.parse_args()
+
+    # Resume last query
+    if options.res:
+        print("--- Resuming last query ---")
+        with open("log.txt", "r") as log_file:
+            # Get pipe_id, dataset, date range
+            query = log_file.readline().split("|")
+            pipe_id = query[1].strip().split(":")[1]
+            dataset = query[2].strip().split(":")[1]
+            start_date = query[3].strip().split(":")[1]
+            end_date = query[4].strip().split(":")[1][:-1]
+            # Make the string into a datetime
+            month, day, year = map(int, start_date.split("/"))
+            start_date = datetime.date(year, month, day)
+            month, day, year = map(int, end_date.split("/"))
+            end_date = datetime.date(year, month, day)
+            dates = [start_date, end_date]
+    # Else remove old log file and generate new one
+    else:
+        try:
+            os.remove("log.txt")
+        except:
+            pass
+        finally:
+            with open("log.txt", "w") as log_file:
+                log_file.close()
+
     # List of strings for status of run
     log = []
 
-    # Select pipeline
-    pipe_id = input("Enter pipeline ID: ")
-    # Check if int
-    try:
-        int(pipe_id)
-    except ValueError:
-        raise ValueError("The pipeline id you entered is not an integer.")
+    if options.res is False:
+        # Select pipeline
+        pipe_id = input("Enter pipeline ID: ")
+        # Check if int
+        try:
+            int(pipe_id)
+        except ValueError:
+            raise ValueError("The pipeline id you entered is not an integer.")
+        # Select dataset
+        dataset = input("Select dataset (opavail / gas_quality / no_notice etc.): ")
+        if dataset not in ["opavail", "no_notice", "gas_quality", "segment_capacity", "index_of_customers"]:
+            raise Exception("Select a dataset to force fetch (opavail, no_notice, gas_quality, segment_capacity, index_of_customers)")
+        # Get the date range
+        dates = dateRangeGenerator()
 
-    # Select dataset
-    dataset = input("Select dataset (opavail / gas_quality / no_notice etc.): ")
-    if dataset not in ["opavail", "no_notice", "gas_quality", "segment_capacity", "index_of_customers"]:
-        raise Exception("Select a dataset to force fetch (opavail, no_notice, gas_quality, segment_capacity, index_of_customers)")
+        # Write query data to log.txt
+        with open("log.txt", "w") as log_file:
+            log_file.write("run_date:{0} | pipe_id:{1} | dataset:{2} | start_date:{3} | end_date:{4}\n".format(datetime.date.today(), pipe_id, dataset, dates[0], dates[1]))
 
-    # Get the date range
-    dates = dateRangeGenerator()
-
+    # Run driver
     try:
         # Start a Chrome driver
         driver = webdriver.Chrome("drivers/chromedriver.exe")
@@ -175,6 +212,7 @@ if __name__ == '__main__':
                 time.sleep(0.1)
                 # Switch to top-most content and continue
                 driver.switch_to_default_content()
+                print("Fetcher found no data - continuing...")
                 continue
             elif driver.find_element_by_class_name("statusMessageSuccess").text is "Fetcher Error":
                 raise IOError("Fetcher encountered an error.")  # Change this ??
@@ -198,7 +236,11 @@ if __name__ == '__main__':
             # Select close button (last anchor tag) and close modal window
             close_button = driver.find_elements_by_tag_name("a")[-1]
             close_button.click()
-            time.sleep(1)
+            time.sleep(0.2)
+            # Add job status to the text log
+            if options.res is False:
+                with open("log.txt", "a") as log_file:
+                    log_file.write(log[-1] + "\n")
         
         # Print successful and unsuccessful jobs
         print("\nSuccessful Jobs:")
